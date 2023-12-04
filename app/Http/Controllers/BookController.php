8dadf67046f3af1be8d227cdea7432cbf456779a
livroad <takaki0106kondo@icloud.com>
2023-12-05 00:01:14 +0900

    create index.blade.php and delete delete each template

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,28 +8,60 @@ use Illuminate\Support\Facades\File;
 
 class BookController extends Controller
 {
-    public function index($type)
+    public function index($type, Request $request)
     {
         $cacheKey = 'json_data_' . $type;
         $jsonDataPath = public_path($type . '.json');
 
-        // キャッシュが存在するかチェック
-        if (Cache::has($cacheKey)) {
-            // キャッシュがある場合はそれを使用
-            $datas = Cache::get($cacheKey);
-        } else {
-            // キャッシュがない場合はJSONファイルからデータを取得
-            $jsonData = File::get($jsonDataPath);
-            $datas = json_decode($jsonData, true);
-
-            // データをキャッシュに保存し、期限を設定（例: 60分）
-            Cache::put($cacheKey, $datas, 60);
+        try {
+            // キャッシュが存在するかチェック
+            $datas = Cache::remember($cacheKey, 60, function () use ($jsonDataPath) {
+                // キャッシュがない場合はJSONファイルからデータを取得
+                $jsonData = File::get($jsonDataPath);
+                return json_decode($jsonData, true);
+            });
+        } catch (\Exception $e) {
+            // エラーハンドリング: ファイルの読み込みやJSONのデコードでエラーが発生した場合の処理
+            return response()->json(['error' => 'Failed to load data.'], 500);
         }
 
+        $keyElements = $this->getKeyElements($type);
+
+        // フィルタリング
+        $filteredDatas = $this->filterData($datas, $request->input('category'));
+
         $imagePath = 'images/noimage.png';
-        $one_data = array_keys($datas[0]["attributes"]);
 
         // 取得したデータを対応するビューに渡して表示
-        return view('books.' . $type, compact('datas', 'imagePath'));
+        return view('books.index', compact('filteredDatas', 'imagePath', 'type', 'keyElements'));
+    }
+
+    private function filterData($datas, $category)
+    {
+        if (!$category) {
+            return $datas; // フィルターが指定されていない場合は全データを返す
+        }
+
+        // フィルターに一致するデータを抽出
+        $filteredDatas = array_filter($datas, function ($data) use ($category) {
+            // ここで適切なフィルターロジックを実装
+            return $data['attributes']['category'] === $category;
+        });
+
+        return array_values($filteredDatas); // インデックスを振り直して返す
+    }
+
+    private function getKeyElements($type)
+    {
+        switch ($type) {
+            case 'characters':
+                return ['image', 'name'];
+            case 'movies':
+                return ['poster', 'title'];
+            case 'spells':
+                return ['image', 'name'];
+            default:
+                return ['defaultImageKey', 'defaultNameKey'];
+        }
     }
 }
--- resources/views/books/movies.blade.php
+++ resources/views/books/movies.blade.php
@@ -1,26 +1,47 @@
 <x-app-layout>
-    <div class="flex flex-wrap justify-between mx-auto py-8 w-4/5">
-        @foreach($datas as $data)
-            <div class="w-1/4 p-4 bg-orange-100 border-double border-4 border-black">
-                <div class="font-bold">
-                    {{ $data["attributes"]["slug"] }}
-                </div>
-                @if($data["attributes"]["poster"])
-                    <img src="{{ $data['attributes']['poster'] }}" alt="noimage" class=" w-44 h-44 object-cover">
-                @else
-                    <img src="{{ asset($imagePath) }}" alt="noimage" class="w-44 h-44 object-cover">
-                @endif
+    <div class="my-4">
+        <form id="filterForm" method="GET" action="{{ route('books', ['type' => $type]) }}">
+            <label for="category" class="sr-only">カテゴリ:</label>
+            <select id="category" name="category" onchange="document.getElementById('filterForm').submit()">
+                <option value="">全てのカテゴリ</option>
+                <option value="Charm" @if(request('category')==='Charm') selected @endif>Charm</option>
+                <option value="Spell" @if(request('category')==='Spell') selected @endif>Spell</option>
+                <!-- 他のカテゴリのオプションを追加 -->
+            </select>
+        </form>
+    </div>
 
-                @foreach($data["attributes"] as $key => $value)
-                    @if($key === 'wiki')
-                        <div><a href="{{ $value }}" class="text-blue-500 hover:underline">wiki</a></div>
-                    @elseif(is_array($value))
-                        <div class="overflow-hidden whitespace-nowrap overflow-ellipsis border-b-2 border-black">{{ $key }} : {{ implode(', ', $value) }}</div>
-                    @else
-                        <div class="overflow-hidden whitespace-nowrap overflow-ellipsis border-b-2 border-black">{{ $key }} : {{ $value }}</div>
-                    @endif
-                @endforeach
+
+    <div class="flex flex-wrap justify-between mx-auto py-8 w-4/5">
+        @foreach($filteredDatas as $data)
+        <div class="w-1/4 p-4 bg-orange-100 border-double border-4 border-black">
+            <div class="font-bold">
+                {{ $data["attributes"][$keyElem[1]] }}
             </div>
+            @if($data["attributes"][$keyElem[0]])
+            <img src="{{ $data['attributes'][$keyElem[0]] }}" alt="noimage" class=" w-44 h-44 object-cover">
+            @else
+            <img src="{{ asset($imagePath) }}" alt="noimage" class="w-44 h-44 object-cover">
+            @endif
+
+            @foreach($data["attributes"] as $key => $value)
+            @if($key === 'wiki')
+            <div><a href="{{ $value }}" class="text-blue-500 hover:underline">wiki</a></div>
+            @elseif(is_array($value))
+            <div class="overflow-hidden whitespace-nowrap overflow-ellipsis border-b-2 border-black">{{ $key }} : {{ implode(', ', $value) }}</div>
+            @else
+            <div class="overflow-hidden whitespace-nowrap overflow-ellipsis border-b-2 border-black">{{ $key }} : {{ $value }}</div>
+            @endif
+            @endforeach
+        </div>
         @endforeach
     </div>
 </x-app-layout>
+
+@push('scripts')
+    <script>
+        document.getElementById('category').addEventListener('change', function() {
+            document.getElementById('filterForm').submit();
+        })
+    </script>
+@endpush
